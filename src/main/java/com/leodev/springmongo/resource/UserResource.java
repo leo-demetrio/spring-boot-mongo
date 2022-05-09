23a8897768cecf2a2886aae81d2cde918028f903
leo-demetrio <leocdemetrio@gmail.com>
2022-05-09 11:21:51 -0300

    Entity User and REST

---
+++ src/main/java/com/leodev/springmongo/resource/UserResource.java
@@ -0,0 +1,25 @@
+package com.leodev.springmongo.resource;
+
+import com.leodev.springmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User m = new User("1", "Maria", "maria@gmail.com");
+        User a = new User("2", "Alex", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(m, a));
+        return ResponseEntity.ok().body(list);
+    }
+}
