d90661f9f04bce3092d6443602cb9fdf168c4159
leo-demetrio <leocdemetrio@gmail.com>
2022-05-09 12:12:01 -0300

    Lombok added

--- src/main/java/com/leodev/springmongo/services/UserService.java
+++ src/main/java/com/leodev/springmongo/services/UserService.java
@@ -2,20 +2,17 @@ package com.leodev.springmongo.services;
 
 import com.leodev.springmongo.domain.User;
 import com.leodev.springmongo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserRepository userRepository;
 
     public List<User> findAll(){
         return this.userRepository.findAll();
