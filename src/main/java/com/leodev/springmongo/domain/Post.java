f05a1119ed9898fe800ab3ba20bad106feb713e0
leo-demetrio <leocdemetrio@gmail.com>
2022-05-13 12:26:00 -0300

    Entity post with user nested

---
+++ src/main/java/com/leodev/springmongo/domain/Post.java
@@ -0,0 +1,22 @@
+package com.leodev.springmongo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@Document
+public class Post implements Serializable {
+    private static final long serialVersion = 1L;
+    @Id
+    private String id;
+    private Date date;
+    private String title;
+    private String body;
+    private User author;
+}
