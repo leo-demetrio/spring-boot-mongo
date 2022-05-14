eff291c7117719db826e04d5ede59cdb5aa0c474
leo-demetrio <leocdemetrio@gmail.com>
2022-05-14 11:10:09 -0300

    Post by id

---
+++ src/main/java/com/leodev/springmongo/services/PostService.java
@@ -0,0 +1,20 @@
+package com.leodev.springmongo.services;
+
+import com.leodev.springmongo.domain.Post;
+import com.leodev.springmongo.exception.ObjectNotFoundException;
+import com.leodev.springmongo.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+    private final PostRepository postRepository;
+    public Post findById(String id){
+        Optional<Post> post = postRepository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Post not found"));
+    }
+}
