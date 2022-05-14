c918d7e6b59d37f2a9691e395ebb0a136b96a50e
leo-demetrio <leocdemetrio@gmail.com>
2022-05-14 15:44:39 -0300

    Method findByTitle

--- src/main/java/com/leodev/springmongo/resource/PostResource.java
+++ src/main/java/com/leodev/springmongo/resource/PostResource.java
@@ -1,13 +1,14 @@
 package com.leodev.springmongo.resource;
 
 import com.leodev.springmongo.domain.Post;
+import com.leodev.springmongo.resource.util.URL;
 import com.leodev.springmongo.services.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +19,10 @@ public class PostResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id){
         return ResponseEntity.ok().body(postService.findById(id));
+    }
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        return ResponseEntity.ok().body(postService.findByTitle(text));
     }
 }
